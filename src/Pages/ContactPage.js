d77eaa9d8e07020050d1748d9add92c0d959cd2e
VanesMP <vanessa.MP@outlook.fr>
2022-09-20 22:23:37 +0200

    add logo/link to my linkedin profil

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,11 +1,17 @@
 import '../style/contactPage.css';
 import Menu from "../Component/Menu";
+import linkIn from "../assets/linkContactIn.png";
 
 function ContactPage() {
 
     return(
         <div className="backgroundContactpage">
                 <Menu styleActive='containerMenuContact'/>
+                <div className='containerLinkIn'>
+                    <a href="www.linkedin.com/in/vanessa-macedo-pinto-devJunior">
+                        <img src={linkIn} alt="link to my linkedin profil, contact me" className='logoIn'/>
+                    </a>
+                </div>
                 <div>
 
                 </div>
